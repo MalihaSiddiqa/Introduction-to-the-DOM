b5d21b7e12106934c71101df30f93d0cf1274413
MalihaSiddiqa <malihasiddiqa6@gmail.com>
2026-06-15 21:06:26 +0530

    Update script.js

--- Manipulating Dom Elements -1/script.js
+++ Manipulating Dom Elements -1/script.js
@@ -1,7 +1,19 @@
-let tabdata=document.getElementById("multiplictionTable");
-let tabrow=document.createElement("tr");
-let tabhead=document.createElement("th");
+let table=document.getElementById("multiplicationTable");
 
-let tabbody=document.createElement("tbody");
-tabdata.appendChild([tabrow,tabhead,tabbody]);
-for
+
+const headerRow = table.insertRow();
+  const th1 = document.createElement("th");
+  th1.textContent = "Number";
+  const th2 = document.createElement("th");
+  th2.textContent = "x5 Result";
+  headerRow.appendChild(th1);
+  headerRow.appendChild(th2);
+
+for (let i = 1; i<=10; i++) {
+    const row=table.insertRow();
+    const column1=row.insertCell();
+    const column2=row.insertCell();
+
+    column1.textContent=i;
+    column2.textContent=i*5;
+}
